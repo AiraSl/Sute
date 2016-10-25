ad0680fa592b6d4f9a5ba005c51c23f8f141485e
Ira <iryna.sliuzar@gmail.com>
2016-10-25 19:29:54 +0300

    airas first commit

---
+++ javaScriptt.js
@@ -0,0 +1,8 @@
+function whCalc (element){
+	var fullId = "#"+element;
+	var width = $("fullId").width();
+	var height =$("fullId").height();
+	$("#for results").("the height of the element textis " + height + " and the width  is " +
+	width");
+}
+whCalc("div_form_2");
